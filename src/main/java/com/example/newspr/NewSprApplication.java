dbe917c936641a0d8623765120f97409de31d12a
Sai Krishna <saikrishnan191@gmail.com>
2023-03-02 15:26:07 -0500

    main class midified

--- src/main/java/com/example/newspr/NewSprApplication.java
+++ src/main/java/com/example/newspr/NewSprApplication.java
@@ -8,6 +8,10 @@ public class NewSprApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NewSprApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(Application.class,args);
+
+        run.close();
+
     }
 
 }
