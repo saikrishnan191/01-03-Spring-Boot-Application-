bd7a7c6cb9a8fb7b6071cae7d5a6a26381efc349
Sai Krishna <saikrishnan191@gmail.com>
2023-03-02 14:24:31 -0500

    This is main project  inital commit

---
+++ src/main/java/com/example/newspr/NewSprApplication.java
@@ -0,0 +1,13 @@
+package com.example.newspr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSprApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewSprApplication.class, args);
+    }
+
+}
