1fad5f2ebfe6fb069743e1d20848f8aa2b0635a5
Sai Krishna <saikrishnan191@gmail.com>
2023-03-05 19:34:20 -0500

    added operator code in bankproject file

--- src/main/java/BankProject.java
+++ src/main/java/BankProject.java
@@ -4,6 +4,15 @@ public class BankProject {
     public void start(){
 
 
-        //kgjkdfgfd
+        int a = 10;
+        int b = 20;
+
+
+       int c= a+b;
+
+      System.out.println(c);
+
+
+
     }
 }
